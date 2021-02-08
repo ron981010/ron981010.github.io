5f76f17409d02edd254c151ac939e04e1d8dd69d
ron981010 <rcacarapi.98@gmail.com>
2021-02-08 05:42:18 -0400

    js

--- lesson5/js/scripts.js
+++ lesson5/js/scripts.js
@@ -2,3 +2,11 @@ function toggleMenu() {
     document.getElementById("primaryNav").classList.toggle("hide");
 
 }
+
+let banner = document.getElementById("banner");
+if (d.getDay() === 6) {
+        banner.style.display = "block";
+} else {
+        banner.style.display = "none";
+}
+
