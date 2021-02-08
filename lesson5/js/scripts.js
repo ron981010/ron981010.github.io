ff6d0cd8e15a9906d401c25608a634b33239bb6d
ron981010 <rcacarapi.98@gmail.com>
2021-02-08 05:45:50 -0400

    more det

--- lesson5/js/scripts.js
+++ lesson5/js/scripts.js
@@ -10,3 +10,34 @@ if (d.getDay() === 6) {
         banner.style.display = "none";
 }
 
+let daynames = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+];
+let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+];
+
+let d  = new Date();
+let hours       = d.getHours();
+let minutes     = d.getMinutes();
+let dayName     = daynames[d.getDay()];
+let monthName   = months[d.getMonth()];
+let year        = d.getFullYear();
+
