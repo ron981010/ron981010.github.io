0e423de0aca76838fc75a68370dde278b9825a83
ron981010 <rcacarapi.98@gmail.com>
2021-03-21 05:59:05 -0400

    better

--- lesson11/jssoda/eventssoda.js
+++ lesson11/jssoda/eventssoda.js
@@ -1,36 +1,102 @@
-const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
-var soda = document.querySelector('.sodacard');
- 
+const requestURL = "https://byui-cit230.github.io/weather/data/towndata.json";
 
 fetch(requestURL)
   .then(function (response) {
     return response.json();
   })
   .then(function (jsonObject) {
-   /* console.table(jsonObject);  temporary checking for valid response and data parsing */
-    const town = jsonObject['towns'];
- 
-   
-   let ul = document.createElement('ul');
-   soda.appendChild(ul);
+    //console.table(jsonObject);  // temporary checking for valid response and data parsing
+    const towns = jsonObject['towns'];
+    for (let i = 0; i < towns.length; i++) {
+      if (i == 2) {
+        let card = document.createElement('section');
+        let event1 = document.createElement('p');
+        let event2 = document.createElement('p');
+        let event3 = document.createElement('p');
+        let event4 = document.createElement('p');
 
-    for (let i = 0; i < town.length; i++ ) { 
+        event1.textContent = towns[i].events[0];
+        event2.textContent = towns[i].events[1];
+        event3.textContent = towns[i].events[2];
+        event4.textContent = towns[i].events[3];
+    
+        card.appendChild(event1);
+        card.appendChild(event2);
+        card.appendChild(event3);
+        card.appendChild(event4);
+        
       
-       if (town[i].name == "Soda Springs"){
-     
+        document.querySelector('div.output').appendChild(card);
+        
+      }
+    }
 
-           for (let j=0; j<town[i].events.length;j++){
-            let li=document.createElement('li');    
-             li.innerHTML = town[i].events[j];
-             ul.appendChild(li);
+  });
+
+  //preston
+  fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    //console.table(jsonObject);  // temporary checking for valid response and data parsing
+    const towns = jsonObject['towns'];
+    for (let i = 0; i < towns.length; i++) {
+      if (i == 6) {
+        let card = document.createElement('section');
+        let event1 = document.createElement('p');
+        let event2 = document.createElement('p');
+        let event3 = document.createElement('p');
+        let event4 = document.createElement('p');
+
+        event1.textContent = towns[i].events[0];
+        event2.textContent = towns[i].events[1];
+        event3.textContent = towns[i].events[2];
+        event4.textContent = towns[i].events[3];
+    
+        card.appendChild(event1);
+        card.appendChild(event2);
+        card.appendChild(event3);
+        card.appendChild(event4);
+        
+      
+        document.querySelector('div.event').appendChild(card);
+        
+      }
+    }
+
+  });
+
+    //soda springs
+    fetch(requestURL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (jsonObject) {
+      //console.table(jsonObject);  // temporary checking for valid response and data parsing
+      const towns = jsonObject['towns'];
+      for (let i = 0; i < towns.length; i++) {
+        if (i == 0) {
+          let card = document.createElement('section');
+          let event1 = document.createElement('p');
+          let event2 = document.createElement('p');
+          let event3 = document.createElement('p');
+          let event4 = document.createElement('p');
+  
+          event1.textContent = towns[i].events[0];
+          event2.textContent = towns[i].events[1];
+          event3.textContent = towns[i].events[2];
+          event4.textContent = towns[i].events[3];
+      
+          card.appendChild(event1);
+          card.appendChild(event2);
+          card.appendChild(event3);
+          card.appendChild(event4);
+          
+        
+          document.querySelector('div.result').appendChild(card);
+          
         }
-
-
-       }
-      
-       
-    }   
-
-
-});
-
+      }
+  
+    });
